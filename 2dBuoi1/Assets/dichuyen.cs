ca09c8749790468a8887e141661c81a7f41fefeb
Nguyễn Anh Hào <contactnguyenanhhao@gmail.com>
2025-05-12 21:21:04 +0700

    học125

--- 2dBuoi1/Assets/dichuyen.cs
+++ 2dBuoi1/Assets/dichuyen.cs
@@ -1,18 +1,46 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class dichuyen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float acceleration = 10f;
+    public float deceleration = 10f;
+    public float maxSpeed = 5f;
+    public float rotationSpeed = 720f; // độ/giây
 
-    // Update is called once per frame
-    void Update()
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Vector2 currentVelocity;
+
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector2(moveX, moveY).normalized;
+    }
+    private void FixedUpdate()
+    {
+        if (moveInput != Vector2.zero)
+        {
+            currentVelocity = Vector2.MoveTowards(currentVelocity, moveInput * maxSpeed, acceleration * Time.fixedDeltaTime);
+
+            // Quay đầu xe theo hướng di chuyển
+            float angle = Mathf.Atan2(currentVelocity.y, currentVelocity.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            currentVelocity = Vector2.MoveTowards(currentVelocity, Vector2.zero, deceleration * Time.fixedDeltaTime);
+        }
+
+        rb.velocity = currentVelocity;
     }
 }
